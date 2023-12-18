a9b1fcc5feb20511688761c8d1dce11f87f14dc6
Pavel Lipskiy <lipskiy.p@gmail.com>
2023-12-18 16:08:47 +0600

    added metrics.

--- dashboard.py
+++ dashboard.py
@@ -13,10 +13,25 @@ class TradingDashboard(BaseDashboard):
     """
     Class to caculate metrics for financial timeseries.
     """
+
     DAYS_YEAR = 252
+
 
     def __init__(self, ts: DataFrame):
         self.ts = ts
+
+
+    def avg_ds(
+        self,
+        offset: int = 1,
+    ) -> List[float]:
+        """
+        Return average price change.
+        """
+        return (
+            self.ts.shift(offset) - self.ts
+        ).mean()
+
 
     def avg_r(
         self,
@@ -26,11 +41,12 @@ class TradingDashboard(BaseDashboard):
         """
         Returns average returns.
         """
-        avg_rs = self._ln_r(offset).mean().values
+        avg_rs = self._ln_r(offset).mean()
 
         if annualise:
             avg_rs *= self.DAYS_YEAR/offset
         return avg_rs
+
 
     def cagr(
         self,
@@ -41,7 +57,8 @@ class TradingDashboard(BaseDashboard):
         """
         _cumprod = (1 + self._ln_r(offset)).cumprod()
         _power = (len(_cumprod.index)) / self.DAYS_YEAR
-        return np.power(_cumprod.values[-1], _power) - 1
+        return _cumprod.iloc[-1] ** _power - 1
+
 
     def sigma_r(
         self,
@@ -51,14 +68,15 @@ class TradingDashboard(BaseDashboard):
         """
         Returns standard deviation of returns.
         """
-        _rs = self._ln_r(offset) ** 2
+        _rs = self._ln_r(offset)
         sigma = np.sqrt(
-            _rs.sum() / (len(_rs) - 1)
-        ).values
+            ( (_rs - _rs.mean()) ** 2).sum() / (len(_rs) - 1)
+        )
 
         if annualise:
             sigma *= np.sqrt(252 / offset)
         return sigma
+
 
     def downside_sigma_r(
         self,
@@ -72,11 +90,12 @@ class TradingDashboard(BaseDashboard):
         _rs = self._ln_r(offset).map(
             lambda x: np.min([x - threshold, 0]) ** 2
         )
-        downside_sigma = np.sqrt(_rs.sum()/len(_rs)).values
+        downside_sigma = np.sqrt( _rs.sum() / len(_rs) )
 
         if annualise:
             downside_sigma *= np.sqrt(252 / offset)
         return downside_sigma
+
 
     def upside_sigma_r(
         self,
@@ -90,8 +109,48 @@ class TradingDashboard(BaseDashboard):
         _rs = self._ln_r(offset).map(
             lambda x: np.max([x - threshold, 0]) ** 2
         )
-        upside_sigma = np.sqrt(_rs.sum()/len(_rs)).values
+        upside_sigma = np.sqrt(_rs.sum()/len(_rs))
 
         if annualise:
             upside_sigma *= np.sqrt(252 / offset)
         return upside_sigma
+
+
+    def covar_r(
+        self,
+        benchmark: str,
+        offset: int = 1,
+        annualise: bool = True,
+    ):
+        """
+        Returns covariance between time-series and benchmark.
+        """
+        _rs = self._ln_r(offset)
+
+        covar = (
+            (
+                (_rs - _rs.mean()) * (_rs[[benchmark]] - _rs[[benchmark]].mean()).values
+            ).sum() / (len(_rs) - 1)
+        )
+
+        if annualise:
+            covar *= 252 / offset
+        return covar
+
+
+    def corr_r(
+        self,
+        benchmark: str,
+        offset: int = 1,
+        annualise: bool = True,
+    ):
+        """
+        Returns correlation between time-series and benchmark.
+        """
+        covar = self.covar_r(benchmark, offset)
+        sigma = self.sigma_r(offset)
+
+        return covar / (sigma * sigma[[benchmark]].values)
+
+
+
--- main.py
+++ main.py
@@ -2,19 +2,48 @@
 Execution file.
 """
 
+import pandas as pd
 
 from dashboard import TradingDashboard
 from timeseries import TimeSeriesFactory
 
 
 if __name__ == "__main__":
-    ts = TimeSeriesFactory.get_random_daily_ts(calendar="NYSE")
+    # Get time-series
+    benchmark = TimeSeriesFactory.get_random_daily_ts(calendar="NYSE")
+    ts_1 = TimeSeriesFactory.get_random_daily_ts(calendar="NYSE")
+    ts_2 = TimeSeriesFactory.get_random_daily_ts(calendar="NYSE")
+
+    # Concat time-series
+    ts = pd.concat([benchmark, ts_1, ts_2], axis=1, join="outer")
+    ts.columns = ["benchmark", "ts_1", "ts_2"]
+
+    # Initialise dashboard
     dashboard = TradingDashboard(ts=ts)
 
-    print({
-        "Avg. Annual Return": dashboard.avg_r()[0],
-        "CAGR": dashboard.cagr()[0],
-        "Vol": dashboard.sigma_r()[0],
-        "DownsideVol": dashboard.downside_sigma_r(threshold=0.01)[0],
-        "UpsideVol": dashboard.upside_sigma_r(threshold=0.01)[0],
-    })
+    res = pd.concat(
+        [
+            dashboard.avg_ds(),
+            dashboard.avg_r(),
+            dashboard.cagr(),
+            dashboard.sigma_r(),
+            dashboard.downside_sigma_r(threshold=0.01),
+            dashboard.upside_sigma_r(threshold=0.01),
+            dashboard.covar_r(benchmark="benchmark"),
+            dashboard.corr_r(benchmark="benchmark"),
+        ],
+        axis=1
+    )
+
+    res.columns = [
+        "Avg. Price Change",
+        "Avg. Annual Return",
+        "CAGR",
+        "Vol",
+        "DownsideVol",
+        "UpsideVol",
+        "CoVar",
+        "Corr"
+    ]
+
+    print(res)
