3a1eb202a556b2d5870b60ab6842db3e980cbcc7
Pavel Lipskiy <lipskiy.p@gmail.com>
2024-03-14 00:39:57 +0600

    further refactoring

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -17,6 +17,8 @@ class TradingDashboard:
 
     def __init__(self, ts: DataFrame):
         self.ts = TimeSeries(ts)
+        self.core_metrics = CoreMetrics(self.ts)
+        self.tail_metrics = TailMetrics(self.ts)
 
     @property
     def metrics(self) -> DataFrame:
@@ -26,22 +28,22 @@ class TradingDashboard:
         _metrics = {
             "Avg. Price Change": self.ts.avg_ds(),
             "Avg. Annual Return": self.ts.avg_r(),
-            "CAGR": CoreMetrics.cagr(self.ts),
-            "Var": CoreMetrics.var(self.ts),
-            "Vol": CoreMetrics.sigma(self.ts),
-            "DownsideVol": CoreMetrics.downside_sigma(self.ts, threshold=0.01),
-            "UpsideVol": CoreMetrics.upside_sigma(self.ts, threshold=0.01),
-            "CoVar": CoreMetrics.covar(self.ts, benchmark="benchmark"),
-            "Corr": CoreMetrics.corr(self.ts, benchmark="benchmark"),
-            "Skew": TailMetrics.skew(self.ts),
-            "CoSkew": TailMetrics.coskew(self.ts, benchmark="benchmark"),
-            "Kurt": TailMetrics.kurtosis(self.ts),
-            "CoKurt": TailMetrics.cokurtosis(self.ts, benchmark="benchmark"),
-            "DD": TailMetrics.drawdown(self.ts),
-            "DDur": TailMetrics.drawdown_dur(self.ts),
-            "MaxDD": TailMetrics.maxdrawdown(self.ts),
-            "MaxDDur": TailMetrics.maxdrawdown_dur(self.ts),
-            "PainIdx": TailMetrics.pain_index(self.ts),
+            "CAGR": self.core_metrics.cagr(),
+            "Var": self.core_metrics.var(),
+            "Vol": self.core_metrics.sigma(),
+            "DownsideVol": self.core_metrics.downside_sigma(threshold=0.01),
+            "UpsideVol": self.core_metrics.upside_sigma(threshold=0.01),
+            "CoVar": self.core_metrics.covar(benchmark="benchmark"),
+            "Corr": self.core_metrics.corr(benchmark="benchmark"),
+            "Skew": self.tail_metrics.skew(),
+            "CoSkew": self.tail_metrics.coskew(benchmark="benchmark"),
+            "Kurt": self.tail_metrics.kurtosis(),
+            "CoKurt": self.tail_metrics.cokurtosis(benchmark="benchmark"),
+            "DD": self.tail_metrics.drawdown(),
+            "DDur": self.tail_metrics.drawdown_dur(),
+            "MaxDD": self.tail_metrics.maxdrawdown(),
+            "MaxDDur": self.tail_metrics.maxdrawdown_dur(),
+            "PainIdx": self.tail_metrics.pain_index(),
         }
 
         metrics = pd.concat(_metrics.values(), axis=1)
